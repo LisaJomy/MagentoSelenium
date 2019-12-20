a3996859baa480dba9ea2cb7bc93fbc9413d3cff
LisaJomy <56934912+LisaJomy@users.noreply.github.com>
2019-12-20 13:16:38 -0500

    Update RegPage.java

--- src/Test2/RegPage.java
+++ src/Test2/RegPage.java
@@ -3,4 +3,11 @@ class RegPage{
 public static void main(){
 System.out.println();
 }
-}
+public void m2() {
+		System.out.println("m2 method...");	
+	}
+		public void m3() {
+		System.out.println("m3 method...");	
+	}
+  
+ }
